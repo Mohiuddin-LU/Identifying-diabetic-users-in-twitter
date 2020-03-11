b0049afac69360c7a467a621d6c447e1d47b672f
mohiuddin02 <55677245+mohiuddin02@users.noreply.github.com>
2020-03-11 10:40:12 -0400

    Update Using Regular Expression.py

--- Using Regular Expression.py
+++ Using Regular Expression.py
@@ -6,13 +6,14 @@ Created on Tue Mar 10 14:50:05 2020
 """
 
 import re
-def accuracy(count):
-    return (count/lab)*100
+def accuracy(count0, count1):
+    total = count0+count1
+    return (count1/total)*100
 #reading label and text from a separate files
 f = open("text.txt", "r")
 l = open("label.txt", "r")
-c = 0
-lab = 0
+count0 = 0
+count1 = 0
 for i in range(1956):
     sentence = f.readline()
     label = l.readline()
@@ -21,7 +22,7 @@ for i in range(1956):
     r = re.compile(r'\binsulin\b | \bI\b | \bHave\b | \bdiabetes\b | \bmyself\b | \bme\b | \bmy\b', flags=re.I | re.X)
     x = re.findall(r, sentence)
     if(x and label == '1'):
-        c = c + 1
-    if(label == '1'):
-        lab = lab + 1
-print(accuracy(c)) 
+        count1 = count1 + 1
+    if(x and label == '0'):
+        count0 = count0 + 1
+print(accuracy(count0,count1)) 
